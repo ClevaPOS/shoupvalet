bdd2cefe8e14c29a39d33e3bbb2673d4777e1ee3
thelinhuk <thelinhuk@googlemail.com>
2017-01-16 22:50:01 +0000

    authenticate user function

--- ShoUpValet/DBConnection.swift
+++ ShoUpValet/DBConnection.swift
@@ -5,20 +5,60 @@
 //  Created by Van Pham on 05/01/2017.
 //  Copyright © 2017 VanPham. All rights reserved.
 //
-
 import Foundation
+
 
 class DBConnection {
     
-    var host: String?
-    var username: String?
-    var password: String?
     
-    init(dbhost: String, usr: String, pwd: String) {
+    init() {
+    }
+    
+    func authenticateUser(username: String, pwd: String, host: String) {
+        let parameters = ["name": username, "password": pwd] as Dictionary<String, String>
+        let url = URL(string: host)! //change the url
+        let session = URLSession.shared
+        var request = URLRequest(url: url)
         
-        self.host = dbhost
-        self.username = usr
-        self.password = pwd
+        request.httpMethod = "GET"
+        
+        do {
+            request.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted) // pass dictionary to nsdata object and set it as request body
+            
+        } catch let error {
+            print(error.localizedDescription)
+        }
+        
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        
+        //create dataTask using the session object to send data to the server
+        let task = session.dataTask(with: request as URLRequest, completionHandler: { data, response, error in
+            
+            guard error == nil else {
+                return
+            }
+            
+            guard let data = data else {
+                return
+            }
+            
+            do {
+                //create json object from data
+                if let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] {
+                    print(json)
+                    // handle json...
+                }
+                
+            } catch let error {
+                print(error.localizedDescription)
+            }
+        })
+        task.resume()
+
+
+
+        
     }
     
     
